5b2bf01836cc46ec4e609cc594ef005bea982b9d
jhonatan.alves <jhonatan.alves@intelie.com.br>
2022-10-03 21:40:07 -0300

    style: 🎨 Creating GlobalStyle component and calling to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World!</h1>;
+      <GlobalStyle />
+      <h1>Hello World!</h1>
     </ThemeProvider>
   );
 };
