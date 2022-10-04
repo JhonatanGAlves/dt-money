d1ce1ef8dc081c73f60b25cb3ecc4ca70ad4dcec
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-03 23:09:19 -0300

    title: ↩️ Calling Summary component in Transactions component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
 
 export const Transactions = () => {
   return (
     <Container>
       <Header />
+      <Summary />
     </Container>
   );
 };
