334a7442ca221f9716d2b6641fec5e9d66ff4f84
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-03 23:08:33 -0300

    style: 🎨 Defining styles to the Summary component

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,3 +1,40 @@
 import styled from "styled-components";
 
-export const SummaryContainer = styled.section``;
+export const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`;
+
+interface SummaryCardProps {
+  variant?: "blue";
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  padding: 2rem;
+  border-radius: 6px;
+
+  background-color: ${({ theme, variant }) =>
+    variant === "blue" ? theme["blue-700"] : theme["gray-600"]};
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    color: ${({ theme }) => theme["gray-300"]};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+`;
