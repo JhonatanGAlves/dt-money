40a4d37f712684be25e7b2f2be6ac4f0625c982d
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-03 23:08:06 -0300

    feat: 👨🏽‍💻 Populating the structure of Summary component with static data

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,5 +1,35 @@
-import { SummaryContainer } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
 
 export const Summary = () => {
-  return <SummaryContainer></SummaryContainer>;
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#2C558C" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="blue">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
 };
