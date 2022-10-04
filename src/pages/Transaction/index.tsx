951f37abab0451151f0c7e84d7a78cb450df7413
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-03 22:38:17 -0300

    title: ↩️ Calling Header component in Transactions component

--- src/pages/Transaction/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from "./styles";
-
-export const Transaction = () => {
-  return <Container></Container>;
-};
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Header } from "../../components/Header/Header";
+
+export const Transactions = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
