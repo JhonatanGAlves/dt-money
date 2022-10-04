3398d7aee5c17767ccc654c468a53f475fec5c71
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-03 22:36:22 -0300

    style: 🎨 Defining styles to the Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+  padding: 2.5rem 0 7.5rem;
+
+  background-color: ${({ theme }) => theme["gray-900"]};
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+  padding: 0 1.25rem;
+  border: 0;
+  border-radius: 6px;
+  cursor: pointer;
+
+  font-weight: bold;
+
+  color: ${({ theme }) => theme.white};
+  background-color: ${({ theme }) => theme["blue-500"]};
+
+  &:hover {
+    background-color: ${({ theme }) => theme["blue-700"]};
+    transition: background-color 0.2s;
+  }
+`;
