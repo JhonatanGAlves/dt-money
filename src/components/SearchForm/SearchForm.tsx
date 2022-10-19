70f5d7894212d541204aecef51bec8a21ba8b454
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-19 08:38:59 -0300

    feat: 👨🏽‍💻 Populating the structure of SearchForm component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,5 +1,15 @@
+import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 
 export const SearchForm = () => {
-  return <SearchFormContainer></SearchFormContainer>;
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Busque por transações" />
+
+      <button type="submit">
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  );
 };
