49e325dbecbb59d47e21d3be15d2a5a3fb0f69f6
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-19 09:30:48 -0300

    feat: 👨🏽‍💻 Populating the structure of NewTransactionModal component

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,5 +1,27 @@
-import { NtmContent } from "./styles";
+import { CloseButton, NtmContent, Overlay } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "phosphor-react";
 
 export const NewTransactionModal = () => {
-  return <NtmContent></NtmContent>;
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <NtmContent>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </NtmContent>
+    </Dialog.Portal>
+  );
 };
