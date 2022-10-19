0bbf887b1c6ff58198a4b0516e4b48ab05b99064
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-19 08:39:42 -0300

    style: 🎨 Defining styles to the SearchForm component

--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -1,3 +1,45 @@
 import styled from "styled-components";
 
-export const SearchFormContainer = styled.div``;
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    padding: 1rem;
+
+    border-radius: 6px;
+    border: 0;
+
+    color: ${({ theme }) => theme["gray-300"]};
+    background: ${({ theme }) => theme["gray-900"]};
+
+    ::placeholder {
+      color: ${({ theme }) => theme["gray-500"]};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    gap: 0.75rem;
+
+    border: 0;
+    border: 1px solid ${({ theme }) => theme["blue-300"]};
+    border-radius: 6px;
+
+    font-weight: bold;
+
+    background: transparent;
+    color: ${({ theme }) => theme["blue-300"]};
+
+    :hover {
+      border-color: ${({ theme }) => theme["blue-500"]};
+      transition: background-color 0.2s, color 0.2s, border-color 0.2s;
+
+      color: ${({ theme }) => theme["white"]};
+      background: ${({ theme }) => theme["blue-500"]};
+    }
+  }
+`;
