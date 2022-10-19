7a1a868b0254d491589d6ae6cbcdaddb8e0309e1
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-19 08:20:36 -0300

    feat: 👨🏽‍💻 Creating and populating the structure of table with static data

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,35 @@
-import { Container } from "./styles";
+import { PriceHighlight, TableContainer, TableContent } from "./styles";
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
 
 export const Transactions = () => {
   return (
-    <Container>
+    <div>
       <Header />
       <Summary />
-    </Container>
+
+      <TableContainer>
+        <TableContent>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/09/2022</td>
+            </tr>
+          </tbody>
+        </TableContent>
+      </TableContainer>
+    </div>
   );
 };
