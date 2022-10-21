b0a5fa9117a6a75d62f86b3596b67e90a952864e
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-21 10:28:37 -0300

    title: ↩️ Calling the API function and passing the response to the transactions const

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import { PriceHighlight, TableContainer, TableContent } from "./styles";
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
 import { SearchForm } from "../../components/SearchForm/SearchForm";
+import { getTransactions } from "../../api/api";
+
+type Transactions = {
+  id: number;
+  description: "Website development";
+  type: "income" | "outcome";
+  category: string;
+  price: number;
+  createdAt: string;
+};
 
 export const Transactions = () => {
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+
+  useEffect(() => {
+    getTransactions().then((data) => setTransactions(data));
+  }, []);
+
   return (
     <div>
       <Header />
@@ -14,22 +31,16 @@ export const Transactions = () => {
 
         <TableContent>
           <tbody>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/09/2022</td>
-            </tr>
+            {transactions.map((t) => (
+              <tr key={t.id}>
+                <td>{t.description}</td>
+                <td>
+                  <PriceHighlight variant={t.type}>{t.price}</PriceHighlight>
+                </td>
+                <td>{t.category}</td>
+                <td>{t.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </TableContent>
       </TableContainer>
