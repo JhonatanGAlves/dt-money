50f966ec724fa19a8e96f962a57b9afce7b17827
jhonatangalves <jhonatangalves96@gmail.com>
2022-10-23 19:07:01 -0300

    title: 📊 Fixing possibles problems of performance

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
 import { PriceHighlight, TableContainer, TableContent } from "./styles";
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
 import { SearchForm } from "../../components/SearchForm/SearchForm";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContextSelector } from "use-context-selector";
 
 export const Transactions = () => {
-  const { transactions } = useContext(TransactionsContext);
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <div>
